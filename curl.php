a1170b05b11c2a63bd29921ada7b1e5a2343199e
Nikos Fazakis <gccster@gmail.com>
2013-02-11 16:02:05 +0200

    added comments

--- curl.php
+++ curl.php
@@ -1,3 +1,4 @@
 <?php	
-	echo passthru("curl ".$_POST['cmd']);
+	echo passthru("curl ".$_POST['cmd']); //this is very dangerous but its just for test 
+										//until a javascript curl implementation can solve the problem
 ?>
