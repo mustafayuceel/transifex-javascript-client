c5023e8c2f68aa6d1e087e9e814e159574267081
mustafayuceel <mustafayuceel@gmail.com>
2013-04-12 10:40:58 +0300

    Update hash.php
    link to documentation part

--- hash.php
+++ hash.php
@@ -1,4 +1,5 @@
 <?php
+	//Here is the documentation: http://help.transifex.com/features/api/#string-hash-algorithm
 	//$_POST['string'] = "Footer here:This is a Content:";
 	echo	hash("md5",utf8_encode($_POST['string']));
 ?>
